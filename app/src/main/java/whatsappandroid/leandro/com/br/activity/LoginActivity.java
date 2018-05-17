570ef1713238c0f47dc2cc1fb67ac7b8ab273f2f
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-17 10:00:24 -0400

    Gerando Token local.

--- app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
+++ app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
@@ -2,10 +2,15 @@ package whatsappandroid.leandro.com.br.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.Random;
 
 import whatsappandroid.leandro.com.br.R;
 
@@ -15,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText nome;
     private EditText codPais;
     private EditText codArea;
+    private Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         nome = findViewById(R.id.edit_nome);
         codPais = findViewById(R.id.edit_cod_pais);
         codArea = findViewById(R.id.edit_cod_area);
+        cadastrar = findViewById(R.id.bt_cadastrar);
 
         /* Definir as Máscaras */
         SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter(" NNNNN-NNNN ");
@@ -39,5 +46,28 @@ public class LoginActivity extends AppCompatActivity {
         SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
         MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
         codArea.addTextChangedListener(maskCodArea);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String nomeUsuario = nome.getText().toString();
+               String telefoneCompleto = codPais.getText().toString() + codArea.getText().toString()
+                       + telefone.getText().toString();
+               String telefoneSemFormatacao = telefoneCompleto.replace("+","");
+               telefoneSemFormatacao = telefoneSemFormatacao.replace("-","");
+               telefoneSemFormatacao = telefoneSemFormatacao.replace(" ","");
+
+                //Log.i("TELEFONE","T: " + telefoneSemFormatacao);
+                // Gerar Token
+                Random randomico = new Random();
+                int numeroRandomico = randomico.nextInt( 8999 ) -1000;
+
+                String token = String.valueOf(numeroRandomico);
+
+                //Log.i("TOKEN","T: " + token);
+
+
+            }
+        });
     }
 }

59c9127d269e0360389dc3f0011f2d26c50af54c
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-18 12:19:39 -0400

    Obtendo permissões com a classe Permissão.

--- app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
+++ app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package whatsappandroid.leandro.com.br.activity;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,7 @@ import java.util.HashMap;
 import java.util.Random;
 
 import whatsappandroid.leandro.com.br.R;
+import whatsappandroid.leandro.com.br.helper.Permissao;
 import whatsappandroid.leandro.com.br.helper.Preferencias;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,11 +26,17 @@ public class LoginActivity extends AppCompatActivity {
     private EditText codPais;
     private EditText codArea;
     private Button cadastrar;
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.INTERNET
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Permissao.validaPermissoes( 1,this, permissoesNecessarias );
 
         telefone = findViewById(R.id.edit_telefone);
         nome = findViewById(R.id.edit_nome);
@@ -65,18 +74,40 @@ public class LoginActivity extends AppCompatActivity {
                 int numeroRandomico = randomico.nextInt(8999) - 1000;
 
                 String token = String.valueOf(numeroRandomico);
+                String mensagemEnvio = "WhatsApp Código de Confirmação: " + token;
 
                 //Log.i("TOKEN","T: " + token);
 
                 Preferencias preferencias = new Preferencias(LoginActivity.this);
                 preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
 
+                //Envio do SMS
+                //telefoneSemFormatacao = "5556"; // ********************************
+                boolean enviadoSMS = enviaSMS( "+" + telefoneSemFormatacao, mensagemEnvio );
+
+                /*
                 HashMap<String, String> usuario = preferencias.getDadosUsuario();
 
                 Log.i("TOKEN", "NOME:" + usuario.get("nome") + "; FONE: " +
                         usuario.get("telefone") + "; TOKEN: " + usuario.get("token"));
+                */
+
+
 
             }
         });
+    }
+
+    /* Envio do SMS */
+    private boolean enviaSMS( String telefone, String mensagem ) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefone, null, mensagem, null,
+                    null);
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }

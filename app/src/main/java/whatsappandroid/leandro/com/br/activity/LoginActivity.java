12a2b736461066a32ca9a0e865c9bf9913f1b289
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-20 11:37:07 -0400

    Mudanças na estrutura e Classe para retornar referência do Firebase.

--- app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
+++ app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
@@ -16,136 +16,33 @@ import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.firebase.database.DatabaseReference;
 
 import java.util.HashMap;
 import java.util.Random;
 
 import whatsappandroid.leandro.com.br.R;
+import whatsappandroid.leandro.com.br.config.ConfiguracaoFirebase;
 import whatsappandroid.leandro.com.br.helper.Permissao;
 import whatsappandroid.leandro.com.br.helper.Preferencias;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText telefone;
-    private EditText nome;
-    private EditText codPais;
-    private EditText codArea;
-    private Button cadastrar;
-    private String[] permissoesNecessarias = new String[]{
-            Manifest.permission.SEND_SMS,
-            Manifest.permission.INTERNET
-    };
+    private DatabaseReference referenciaDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Permissao.validaPermissoes(1, this, permissoesNecessarias);
+        referenciaDatabase = ConfiguracaoFirebase.getFirebase();
+        referenciaDatabase.child("pontos").setValue("800");
 
-        telefone = findViewById(R.id.edit_telefone);
-        nome = findViewById(R.id.edit_nome);
-        codPais = findViewById(R.id.edit_cod_pais);
-        codArea = findViewById(R.id.edit_cod_area);
-        cadastrar = findViewById(R.id.bt_cadastrar);
-
-        /* Definir as Máscaras */
-        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter(" NNNNN-NNNN ");
-        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
-
-        telefone.addTextChangedListener(maskTelefone);
-
-        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter(" +NN ");
-        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
-        codPais.addTextChangedListener(maskCodPais);
-
-        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
-        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
-        codArea.addTextChangedListener(maskCodArea);
-
-        cadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nomeUsuario = nome.getText().toString();
-                String telefoneCompleto = codPais.getText().toString() + codArea.getText().toString()
-                        + telefone.getText().toString();
-                String telefoneSemFormatacao = telefoneCompleto.replace("+", "");
-                telefoneSemFormatacao = telefoneSemFormatacao.replace("-", "");
-                telefoneSemFormatacao = telefoneSemFormatacao.replace(" ", "");
-
-                //Log.i("TELEFONE","T: " + telefoneSemFormatacao);
-                // Gerar Token
-                Random randomico = new Random();
-                int numeroRandomico = randomico.nextInt(8999) + 1000;
-
-                String token = String.valueOf(numeroRandomico);
-                String mensagemEnvio = "WhatsApp Código de Confirmação: " + token;
-
-                //Log.i("TOKEN","T: " + token);
-
-                Preferencias preferencias = new Preferencias(LoginActivity.this);
-                preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
-
-                //Envio do SMS
-                //telefoneSemFormatacao = "5556"; // ********************************
-                boolean enviadoSMS = enviaSMS("+" + telefoneSemFormatacao, mensagemEnvio);
-
-                if(enviadoSMS){
-                    Intent intent = new Intent(LoginActivity.this, ValidadorActivity.class);
-                    startActivity(intent);
-                    finish();
-
-                } else {
-                    Toast.makeText(LoginActivity.this, "Problema ao enviar o SMS, " +
-                            "tente novamente.", Toast.LENGTH_SHORT).show();
-                }
-
-                /*
-                HashMap<String, String> usuario = preferencias.getDadosUsuario();
-
-                Log.i("TOKEN", "NOME:" + usuario.get("nome") + "; FONE: " +
-                        usuario.get("telefone") + "; TOKEN: " + usuario.get("token"));
-                */
-
-
-            }
-        });
     }
 
-    /* Envio do SMS */
-    private boolean enviaSMS(String telefone, String mensagem) {
-        try {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(telefone, null, mensagem, null,
-                    null);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+    public void abrirCadastroUsuario(View view) {
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for (int resultado : grantResults) {
-            if (resultado == PackageManager.PERMISSION_DENIED) {
-                alertaValidacaoPermissao();
-            }
-        }
-    }
-
-    private void alertaValidacaoPermissao() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Permissões Negadas");
-        builder.setMessage("Para utilizar esse app, é necessário aceitar as permissões.");
-        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
-
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/whatsappandroid/leandro/com/br/config/ConfiguracaoFirebase.java
@@ -0,0 +1,20 @@
+package whatsappandroid.leandro.com.br.config;
+
+import android.provider.ContactsContract;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public final class ConfiguracaoFirebase {
+
+    private static DatabaseReference referenciaFirebase;
+
+    public static DatabaseReference getFirebase() {
+
+        if (referenciaFirebase == null) {
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+
+        return referenciaFirebase;
+    }
+}

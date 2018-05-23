15a59b6ac657693af37f879f53a1e01ee3cab8f7
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-23 12:08:24 -0400

    Login de Usuário.

--- app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
+++ app/src/main/java/whatsappandroid/leandro/com/br/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 //import android.support.v7.app.AlertDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.*;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +19,11 @@ import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.database.DatabaseReference;
 
 import java.util.HashMap;
@@ -27,18 +33,75 @@ import whatsappandroid.leandro.com.br.R;
 import whatsappandroid.leandro.com.br.config.ConfiguracaoFirebase;
 import whatsappandroid.leandro.com.br.helper.Permissao;
 import whatsappandroid.leandro.com.br.helper.Preferencias;
+import whatsappandroid.leandro.com.br.model.Usuario;
 
 public class LoginActivity extends Activity {
 
     private DatabaseReference referenciaDatabase;
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        referenciaDatabase = ConfiguracaoFirebase.getFirebase();
-        //referenciaDatabase.child("pontos").setValue("800");
+        verificarUsuarioLogado();
+
+        email = findViewById(R.id.edit_login_email);
+        senha = findViewById(R.id.edit_login_senha);
+        botaoLogar = findViewById(R.id.bt_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                usuario = new Usuario();
+                usuario.setEmail( email.getText().toString() );
+                usuario.setSenha( senha.getText().toString() );
+                validarLogin();
+
+            }
+        });
+
+
+    }
+
+    private void verificarUsuarioLogado() {
+        autenticacao = ConfiguracaoFirebase.getAutenticacao();
+        if(autenticacao.getCurrentUser() != null) {
+            abrirTelaPrincipal();
+        }
+    }
+
+    private void validarLogin() {
+
+        autenticacao = ConfiguracaoFirebase.getAutenticacao();
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if( task.isSuccessful() ) {
+                            abrirTelaPrincipal();
+                            Toast.makeText(LoginActivity.this, "Sucesso ao fazer " +
+                                    "login",Toast.LENGTH_LONG).show();
+                        } else {
+
+                            Toast.makeText(LoginActivity.this, "Erro ao fazer " +
+                                    "login. Verifique e-mail e senha.",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
 
     }
 
@@ -47,4 +110,5 @@ public class LoginActivity extends Activity {
         Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
         startActivity(intent);
     }
+
 }
